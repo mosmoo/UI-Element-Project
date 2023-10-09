5f553c2422b8f3a418165a54495b976572b8fe50
manasvislj <manasvisaluja2003@gmail.com>
2023-10-09 17:30:18 -0400

    Added an api

--- Lib/script.js
+++ Lib/script.js
@@ -1,1 +1,30 @@
-console.log('hello')
+const url ="https://api.imgflip.com/get_memes"
+fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(response => {
+        console.log("success!", response)
+        console.log(response.data.memes[1])
+        processApiData(response)
+    })
+    .catch(error => {
+        console.error("Something went wrong...", error)
+    })
+// const btn = document.getElementById('GenerateButton');
+
+// btn.addEventListener("click", handleClick);
+
+// function handleClick(){
+//   console.log("clicked!")
+// }
+
+function processApiData(apiData){
+    console.log('Processing API data:', apiData);
+    const arr = apiData.data.memes[1];
+    const arr_img = arr.url
+    const acc1 = document.getElementById('acc1');
+    acc1.innerText = arr_img
+}
+
+
